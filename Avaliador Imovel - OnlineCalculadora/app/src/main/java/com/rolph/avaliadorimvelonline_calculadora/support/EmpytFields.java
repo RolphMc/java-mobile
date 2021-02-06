e8f501595e0c236d78deedfa3479e1a5c346f64d
RolphMc <rmc.rodolfo@gmail.com>
2021-02-06 19:07:30 -0300

    deletado arquivos de terceiros

--- Avaliador Imovel - OnlineCalculadora/app/src/main/java/com/rolph/avaliadorimvelonline_calculadora/support/EmpytFields.java
+++
@@ -1,9 +0,0 @@
-package com.rolph.avaliadorimvelonline_calculadora.support;
-
-import android.content.Context;
-import android.widget.EditText;
-import android.widget.Toast;
-
-public class EmpytFields{
-
-}
--- Avaliador Imovel - OnlineCalculadora/app/src/main/java/com/rolph/avaliadorimvelonline_calculadora/view/testes.java
+++
@@ -1,6 +0,0 @@
-package com.rolph.avaliadorimvelonline_calculadora.view;
-
-public class testes {
-    public static void main(String[] args) {
-    }
-}
